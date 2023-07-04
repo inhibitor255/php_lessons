8589ca3cc83a2b7f884fce38a8471e8cfe8a0f00
Kaung Myat <kaungmyatkyaw255@example.com>
2023-07-04 15:31:52 +0630

    Todo list prototype finished

--- todoList/update.php
+++ todoList/update.php
@@ -10,41 +10,50 @@
     <?php
         require_once("./db_connection.php");
         
-        $id = $_GET["id"];
-        $sql = "SELECT * FROM work WHERE id = $id";
-        $query = mysqli_query($conn,$sql);
-        $data = mysqli_fetch_assoc($query); // step 1. get data
+        if (isset($_GET['id'])){
+            $id = $_GET["id"];
+            $sql = "SELECT * FROM work WHERE id = $id";
+            $query = mysqli_query($conn,$sql);
+            $data = mysqli_fetch_assoc($query); // step 1. get data
+
+            if(isset($_POST['updateBtn'])){
+                $taskName = $_POST['taskName'];
+                if($taskName == null || $taskName == "") {
+                    echo "<small style='color: red;'>Task Name is required</small>";
+                }else{
+                    $updateSQL = "UPDATE work SET name = '{$_POST['taskName']}' WHERE work.id = {$_POST['taskId']}";
+                    if(mysqli_query($conn,$updateSQL)){ //step 4. update
+                        header("location:./read.php");
+                    }else{
+                        echo "Update fail...";
+                    }
+    
+                    
+                }
+    
+            };
+            // UPDATE `work` SET `name` = 'dodo' WHERE `work`.`id` = 11;
+        }else{
+            echo "<small style='color: red;'>First Go to Read Page.</small>";
+        }
 
         
     ?>
-
-    <?php
-        if(isset($_POST['updateBtn'])){
-            $taskName = $_POST['taskName'];
-            if($taskName == null || $taskName == "") {
-                echo "<small style='color: red;'>Task Name is required</small>";
-            }else{
-                $updateSQL = "UPDATE work SET name = '{$_POST['taskName']}' WHERE work.id = {$_POST['taskId']}";
-                if(mysqli_query($conn,$updateSQL)){
-                    header("location:./read.php");
-                }else{
-                    echo "Update fail...";
-                }
-
-                
-            }
-
-        };
-        // UPDATE `work` SET `name` = 'dodo' WHERE `work`.`id` = 11;
-    ?>
+    <a href="./read.php">Read Page</a>
     <form action="" method="POST">
+        
         Tasks
-        <input type="hidden" name="taskId" value="<?php echo $data['id']?>" >
-        <input type="text" name="taskName" value="<?php echo $data['name']?>" > <!-- step 2. show -->
+        <input type="hidden" name="taskId" value="<?php echo $data['id']?>" required>
+        <input type="text" name="taskName" value="<?php if (isset($_GET['id'])) {
+            echo $data['name'];
+        }
+        ?>" required > <!-- step 2. show -->
         <button name="updateBtn">Update</button>
+        
     </form>
 </body>
 </html>
+
 
 
 
